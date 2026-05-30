ae2893957c403a4a4055e44a6e62db20d4286cc8
yuvraj <yuvraj.as9001@gmail.com>
2026-05-30 15:05:46 +0530

    ci-cd.yml added

---
+++ src/test/java/com/routefinder/SmartRouteFinderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.routefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRouteFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
